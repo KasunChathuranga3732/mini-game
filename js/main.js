e5bf0dd28aead73ab7a5561b3b6dbd05a079af23
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-19 09:22:57 +0530

    Add character and design its idle state

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 const backElm = document.body.querySelector('#background');
+const boxElm = document.createElement('div');
+const htmlElm = document.body;
+boxElm.classList.add('box');
+backElm.appendChild(boxElm);
+
+
+
+setInterval(()=> {
+    drawIdle();
+}, 40);
+
+
+
+
+
+let idle = 1;
+function drawIdle(){
+    boxElm.style.backgroundImage = `url('img/Idle (${idle++}).png')`;
+    if(idle === 11) idle = 1;
+}
