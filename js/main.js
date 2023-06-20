944548cf75d91161a79340b16fcaa1102eb560fa
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-20 08:35:54 +0530

    Design jump state of character

--- js/main.js
+++ js/main.js
@@ -11,11 +11,34 @@ backElm.appendChild(scoreBoard);
 
 
 
+let jump = false;
 
-setInterval(()=> {
-    drawIdle();
+
+
+document.body.addEventListener('keydown', (eventData)=> {
+    if (eventData.code === 'Space') {
+        jump = true;
+    }
+});
+
+
+
+let a = 0;
+setInterval(()=>{
+    a++;
+    if(!jump){
+        drawIdle();
+    } else if(jump && (a % 2 === 0)){
+        drawJump()
+    }
 }, 40);
 
+
+setInterval(()=> {
+    if (jump) {
+        doJump();
+    }
+},5);
 
 
 
@@ -25,3 +48,28 @@ function drawIdle(){
     boxElm.style.backgroundImage = `url('img/Idle (${idle++}).png')`;
     if(idle === 11) idle = 1;
 }
+
+let dJump = 1;
+function drawJump(){
+    boxElm.style.backgroundImage = `url('img/Jump (${dJump++}).png')`;
+    if(dJump === 11) dJump = 1;
+}
+
+
+
+
+let angle = 0;
+function doJump(){
+    let y  = Math.cos(angle * (Math.PI / 180));
+    y *= 3;
+    boxElm.style.top = (boxElm.offsetTop - y) + "px";
+    angle++;
+    if (angle >  180){
+        jump = false;
+        angle = 0;
+    }
+}
+
+
+
+
